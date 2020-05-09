f7b32e730f4a6f549f069b7f6be9f67b7994bd25
Mirza Jasarevic <mrzjsrvc@hotmail.com>
2020-05-09 07:15:25 +0200

    CVRP fix, removed clutter

--- test.py
+++ test.py
@@ -51,57 +51,37 @@ def print_solution(data, manager, routing, solution):
 
 
 def main():
-    """Solve the CVRP problem."""
-    # Instantiate the data problem.
     data = create_data_model()
-
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                           data['num_vehicles'], data['depot'])
-
-    # Create Routing Model.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),data['num_vehicles'], data['depot'])
     routing = pywrapcp.RoutingModel(manager)
 
+    # ========================================================================
 
-    # Create and register a transit callback.
     def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
+
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
+        print("distance_callback: ",data['distance_matrix'][from_node][to_node])
         return data['distance_matrix'][from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-
-    # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-
-    # Add Capacity constraint.
     def demand_callback(from_index):
-        """Returns the demand of the node."""
-        # Convert from routing variable Index to demands NodeIndex.
+
         from_node = manager.IndexToNode(from_index)
+        print("demand_callback: ",data['demands'][from_node])
         return data['demands'][from_node]
 
-    demand_callback_index = routing.RegisterUnaryTransitCallback(
-        demand_callback)
-    routing.AddDimensionWithVehicleCapacity(
-        demand_callback_index,
-        0,  # null capacity slack
-        data['vehicle_capacities'],  # vehicle maximum capacities
-        True,  # start cumul to zero
-        'Capacity')
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(demand_callback_index, 0, data['vehicle_capacities'], True, 'Capacity')
 
-    # Setting first solution heuristic.
+    # ========================================================================
+
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
-    # Solve the problem.
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
     solution = routing.SolveWithParameters(search_parameters)
 
-    # Print solution on console.
     if solution:
         print_solution(data, manager, routing, solution)
 
